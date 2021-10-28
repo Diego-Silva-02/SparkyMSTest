cebeba73865ee2c986d4385235f018bbec367105
Diego Silva <diegosilva22a@gmail.com>
2021-10-28 16:59:12 -0300

    Creating my first unit test

---
+++ SparkyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sparky;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            // → Test phases
+            //Arrange
+            Calculator calc = new();
+
+            // Act
+            int result = calc.AddNumbers(10, 20);
+
+            // Assert
+            Assert.AreEqual(30, result);
+            // Expect, actual
+        }
+
+        //[TestMethod]
+        //public void AddNumbers_InputTwoInt_GetIncorrectAddition()
+        //{
+        //    // → Test phases
+        //    //Arrange
+        //    Calculator calc = new();
+
+        //    // Act
+        //    int result = calc.AddNumbers(10, 20);
+
+        //    // Assert
+        //    Assert.AreEqual(3, result);
+        //    // Expect, actual
+        //}
+
+    }
+}
