b450cb1ff62d0a159ef965f1b55565b97686efa2
Diego-Silva-02 <diegosilva22a@gmail.com>
2021-11-08 17:24:41 -0300

    Testing the Fibonacci series

---
+++ SparkyNUnitTest/FiboNUnitTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class FiboNUnitTests
+    {
+        private Fibo fibo;
+        [SetUp]
+
+        public void Setup()
+        {
+            fibo = new Fibo();
+        }
+
+        [Test]
+        public void FiboChecker_Input1_ReturnsFiboSeries()
+        {
+            fibo.Range = 1;
+
+            List<int> expectedFiboSeries = new () { 0 };
+
+            List<int> results = fibo.GetFiboSeries();
+
+            Assert.IsNotEmpty(results);
+            Assert.That(results, Is.Ordered);
+            Assert.AreEqual(expectedFiboSeries, results);
+        }
+
+        [Test]
+        public void FiboChecker_Input6_ReturnsFiboSeries()
+        {
+            fibo.Range = 6;
+
+            List<int> expectedFiboSeries = new() { 0, 1, 1, 2, 3, 5 };
+
+            List<int> results = fibo.GetFiboSeries();
+
+            Assert.Contains(3, results);
+            Assert.AreEqual(results.Count, 6);
+            Assert.That(results, Has.No.Member(4));
+            Assert.AreEqual(expectedFiboSeries, results);
+        }
+    }
+}
